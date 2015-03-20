afb3342af3d3ceb3a4f22f1da3eb28c261f0458f
Thomas Pakh <thomas.pakh@gmail.com>
2015-03-19 20:37:14 -0400

    Added Towers of Hanoi.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Towers-of-Hanoi
+Super simple console Towers of Hanoi solver.
+
+*Example:*
+
+```
+Move from Peg 1 to Peg 3
+Move from Peg 1 to Peg 2
+Move from Peg 3 to Peg 2
+Move from Peg 1 to Peg 3
+Move from Peg 2 to Peg 1
+Move from Peg 2 to Peg 3
+Move from Peg 1 to Peg 3
+Move from Peg 1 to Peg 2
+Move from Peg 3 to Peg 2
+Move from Peg 3 to Peg 1
+Move from Peg 2 to Peg 1
+Move from Peg 3 to Peg 2
+Move from Peg 1 to Peg 3
+Move from Peg 1 to Peg 2
+Move from Peg 3 to Peg 2
+```
---
+++ TowersOfHanoi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TowersOfHanoi
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n;
+            bool readKey = args.Length > 1;
+            if (args.Length < 1)
+            {
+                Console.Write("Enter the No. of Disks: ");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            else
+            {
+                n = Convert.ToInt32(args[0]);
+                Console.WriteLine("Number of disks: {0}", n);
+            }
+            for (int x = 1; x < (1 << n); x++)
+            {
+                Console.WriteLine("Move from Peg {0} to Peg {1}", (x & x - 1) % 3 + 1, ((x | x - 1) + 1) % 3 + 1);
+                if (readKey)
+                    Console.ReadKey();
+            }
+        }
+    }
+}
